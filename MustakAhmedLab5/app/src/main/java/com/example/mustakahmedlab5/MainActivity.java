56386b0b7fce844d8368e2cadc479599cf8d713e
Mustak Ahmed <musmak224@gmail.com>
2020-02-14 12:27:29 -0500

    testing git connections

---
+++ MustakAhmedLab5/app/src/main/java/com/example/mustakahmedlab5/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.mustakahmedlab5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //testing to see if git works
+    }
+}
