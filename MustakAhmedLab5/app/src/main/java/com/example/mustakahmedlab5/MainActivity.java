dd16a061b7ae1407e73c048823306d0b74a53505
Mustak Ahmed <musmak224@gmail.com>
2020-02-15 19:26:05 -0500

    lab 5 , implicit intent

--- MustakAhmedLab5/app/src/main/java/com/example/mustakahmedlab5/MainActivity.java
+++ MustakAhmedLab5/app/src/main/java/com/example/mustakahmedlab5/MainActivity.java
@@ -2,14 +2,33 @@ package com.example.mustakahmedlab5;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    String url = "https://developer.android.com";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //testing to see if git works
     }
+
+    public void openBrowser(View view ){
+
+        Uri webpage = Uri.parse(url);
+        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+
+        if(intent.resolveActivity(getPackageManager()) != null){
+            startActivity(intent);
+
+
+
+        }
+
+    }
+
 }
